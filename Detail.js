f2d4f491ed1f2698863640d0173bf65feda68977
shinta132 <104214572+shinta132@users.noreply.github.com>
2022-11-18 22:15:15 +0700

    Add files via upload

---
+++ Detail.js
@@ -0,0 +1,63 @@
+import * as React from 'react';
+import { View,Text,StyleSheet,Button,Image,Linking } from 'react-native'; 
+import LinearGradient from 'react-native-linear-gradient';
+import { Title,Card } from 'react-native-paper';
+import { MaterialIcons,Entypo} from 'react-native-vector-icons';
+import email from '../icon/email.jpg';
+import call from '../icon/call.png';
+import dollar from '../icon/dollar.png';
+
+
+
+const Detail =({navigation,route})=>{
+    const {karyawan}= route.params
+    return(
+        <View style={{flex:1}}>
+            <LinearGradient colors={["#0033ff","#6bc1ff"]} style={{height:"20%"}}/>
+            <View style={{alignItems:"center"}}>
+                <Image style={{width:120,height:120,borderRadius:120/2,marginTop:-50}}
+                source={{uri:karyawan.foto}}/>
+            </View>
+
+            <View style={{alignItems:"center",margin:15}}>
+                <Title>{karyawan.nama}</Title>
+                <Text style={{fontSize:14,color:'black'}}>{karyawan.jabatan}</Text>
+            </View>
+            <Card style={{margin:3}}
+            onPress={()=>{Linking.openURL(`mailto:${karyawan.email}&subject`)}}>
+                <View style={{flexDirection:'row',padding:8}}>
+                    <Image source={email} style={{width:100,height:100}}/>*
+                    <Text style={styles.teks}>{karyawan.email}</Text>
+                </View>
+            </Card>
+            <Card style={{margin:3}}
+            onPress={()=>{Linking.openURL(`tel:${karyawan.telp}`)}}>
+                <View style={{flexDirection:'row',padding:8}}>
+                <Image source={call} style={{width:100,height:100}}/>
+                    <Text style={styles.teks}>{karyawan.telp}</Text>
+                </View>
+            </Card>
+            <Card style={{margin:3,}}>
+                <View style={{flexDirection:'row',padding:8}}>
+                <Image source={dollar} style={{width:100,height:100}}/>
+                    <Text style={styles.teks}>{karyawan.gaji}</Text>
+                </View>
+            </Card>
+            <View style={{padding:10}}>
+                <Button title='Go Back' onPress={()=>navigation.navigate('Home')}/>
+            </View>
+        </View>
+    )
+}
+
+const styles=StyleSheet.create({
+    teks:{
+        fontSize:15,
+        marginTop:3,
+        marginLeft:5,
+        color:'black'
+    }
+});
+
+export default Detail;
+
